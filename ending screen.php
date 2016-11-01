8903e8c75bd7dcd3a66b16336debb8c7a212d992
CaptainWeasle <voroschilin.andre@outlook.de>
2016-11-01 10:39:43 +0100

    ending screen added, readme updated
    ending screen with basic html structure.
    readme with new advice for the Dokumentation

---
+++ ending screen.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <meta charset=utf-8>
+    
+    <title>
+        Ending screen.
+    </title>
+</head>
+
+<body>
+    
+    <header>
+        
+    </header>
+    
+    <main>
+    
+    <p>
+        WAS MÖCHTEN SIE TUN?
+    </p> 
+    
+    <a href=leaderboards.php>
+        LEADERBOARDS!
+    </a>
+    
+    <a href=levelselect.php>
+        NEU STARTEN!
+    </a>
+    
+    <a href=index.php>
+        SPIEL BEENDEN!
+    </a>
+        
+    </main>
+    
+    <aside>
+        <a href>    
+            EINSTELLUNGEN
+        </a>
+    </aside>
+</body>
+
+</html>
